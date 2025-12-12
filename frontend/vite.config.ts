a93c2b3af26847e23ce84d10b3c178bdb90868e1
Faheem Jinna Sulaiman <faheemjinna100@gmail.com>
2025-12-12 05:35:47 -0600

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/MyKaasu/' : '/',
+  base: '/MyKaasu/',
   server: {
     port: 3000,
     proxy: {
